41eaf3abb0d1ee8a129ef59b1fcb5584db1a9c85
Yann LE COZ <yann.lecoz@ynov.com>
2019-03-13 15:19:35 +0100

    Ajout de la TABLE 'films' dans le documents isertData.sql

--- SQL/insertData.sql
+++ SQL/insertData.sql
@@ -1,1 +1,12 @@
- 
+INSERT INTO films (titreFilm, genreFilm, budgetFilm, dureeFilm, paysorigineFilm)
+VALUES
+('Interstellar', 'Science-fiction', 165000000, 169, 'Royaume-Uni'),
+('Dunkerque', 'Guerre', 100000000, 106, 'Royaume-Uni'),
+('The Dark Knight : Le Chevalier noir', 'Super-héros', 185000000, 147, 'Royaume-Uni'),
+('John Rambo', 'Action', 50000000, 91, 'État-Unis'),
+('Projet X', 'Comédie', 12000000, 88, 'État-Unis'),
+('Hannibal', 'Horreur', 87000000, 125, 'État-Unis'),
+('Dragons', 'Animation', 165000000, 93, 'État-Unis'),
+('Doctor Strange', 'Super-héros', 165000000, 147, 'État-Unis'),
+('Invictus', 'Drame', 60000000, 134, 'État-Unis'),
+('Indiana Jones et le Royaume du crâne de cristal', 'Aventure', 125000000, 123, 'État-Unis');
